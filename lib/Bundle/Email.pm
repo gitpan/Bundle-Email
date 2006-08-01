ae2cc6ad44931dc3be710fdcccfb4f017c1f4f11
Ricardo SIGNES <rjbs@cpan.org>
2006-08-01 11:49:36 +0000

    Import of RJBS/Bundle-Email-0.040 from CPAN.
    gitpan-cpan-distribution: Bundle-Email
    gitpan-cpan-version:      0.040
    gitpan-cpan-path:         RJBS/Bundle-Email-0.040.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Bundle/Email.pm
+++ lib/Bundle/Email.pm
@@ -4,7 +4,7 @@ use strict;
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.03';
+  $VERSION = '0.040';
 }
 
 1;
@@ -13,11 +13,11 @@ __END__
 
 =head1 NAME
 
-Bundle::Devel - A bundle to install the main set of Email:: modules
+Bundle::Email - a bundle to install the main set of Email:: modules
 
 =head1 SYNOPSIS
 
-C<perl -MCPAN -e 'install Bundle::Devel'>
+  perl -MCPAN -e 'install Bundle::Devel'
 
 =head1 CONTENTS
 
@@ -77,6 +77,12 @@ Which means that there's a whole bunch of them, all seperate.
 Bundle::Email installs pretty much all of the main Email modules that don't
 have giant cascading dependencies, like L<Email::Store> (which is a
 L<Class::DBI>-based thing).
+
+=head1 PERL EMAIL PROJECT
+
+This bundle is maintained by the Perl Email Project.
+
+  http://emailproject.perl.org/wiki/Bundle::Email
 
 =head1 SUPPORT
 
